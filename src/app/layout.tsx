3a8e415797dfd53d56b039232ee9be949168a1d3
Max Basev <64890011+MaxBasev@users.noreply.github.com>
2025-04-25 15:15:52 +0600

    add Google Analytics script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import SmoothScroll from "@/components/SmoothScroll";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -46,6 +47,23 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
+			<head>
+				<Script
+					strategy="afterInteractive"
+					src="https://www.googletagmanager.com/gtag/js?id=G-J8Z11CEVPG"
+				/>
+				<Script
+					id="google-analytics"
+					strategy="afterInteractive"
+				>
+					{`
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+						gtag('config', 'G-J8Z11CEVPG');
+					`}
+				</Script>
+			</head>
 			<body className={inter.className}>
 				<SmoothScroll />
 				{children}
